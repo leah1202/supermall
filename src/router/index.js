4f38d65a362481a311652074ef1a016148f424f0
leah1202 <li15172346060@163.com>
2020-04-13 15:19:56 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-const Home = () => import('views/home/Home')
-const Category = () => import('views/category/Category')
-const Cart = () => import('views/cart/Cart')
-const Profile = () => import('views/profile/Profile')
-const Detail = () => import('views/detail/Detail')
+const Home = () =>
+    import ('views/home/Home')
+const Category = () =>
+    import ('views/category/Category')
+const Cart = () =>
+    import ('views/cart/Cart')
+const Profile = () =>
+    import ('views/profile/Profile')
+const Detail = () =>
+    import ('views/detail/Detail')
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [
-    {
-      path: '/',
-      redirect: '/home'
-    },
-    {
-      path: '/home',
-      component: Home
-    },
-    {
-      path: '/category',
-      component: Category
-    },
-    {
-      path: '/cart',
-      component: Cart
-    },
-    {
-      path: '/profile',
-      component: Profile
-    },
-    {
-      path: '/detail',
-      component: Detail
-    }
-  ],
-  mode: 'history'
+    routes: [{
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/category',
+            component: Category
+        },
+        {
+            path: '/cart',
+            component: Cart
+        },
+        {
+            path: '/profile',
+            component: Profile
+        },
+        {
+            path: '/detail',
+            component: Detail
+        }
+    ],
+    // mode: 'history'
 })
 
 export default router
