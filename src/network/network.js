520b0186b0615fafa1634852ecfb4dea70e31705
leah1202 <li15172346060@163.com>
2020-04-13 14:27:42 +0800

    complete project

--- src/network/network.js
+++ src/network/network.js
@@ -6,7 +6,8 @@ export function request(options) {
         // 1.创建axios的实例对象
         const instance = axios.create({
             // baseURL: 'http://123.207.32.32:8000',
-            baseURL: 'http://106.54.54.237:8000/api/wh',
+            // baseURL: 'http://106.54.54.237:8000/api/wh',
+            baseURL: "http://152.136.185.210:8000/api/h8",
             timeout: 5000
         })
 
