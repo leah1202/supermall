b1adc8067d42866f0ef3cdece8e11e7c2c61b97f
leah1202 <li15172346060@163.com>
2020-04-13 16:48:47 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    lintOnSave: false,
+    devServer: {
+        open: true,
+        host: "localhost",
+        port: '8080',
+        https: false,
+        hotOnly: false,
+        proxy: {
+            '/api': {
+                target: 'http://152.136.185.210:8000/api/h8',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
     configureWebpack: {
         resolve: {
             alias: {
